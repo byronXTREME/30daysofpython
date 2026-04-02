a107222581681a0455a47d70fe2b9d1aba75ef83
Byron Espinoza <bespinoza@aama.org>
2026-04-02 18:47:51 -0500

    day 2 windows so far

---
+++ day_1/day_2.1.py
@@ -0,0 +1,29 @@
+# int to float
+num_int = 10
+print('num_int' ,num_int)
+num_float = float(num_int)
+print('num_float: ', num_float)
+print()
+# float to int
+gravity = 9.81
+print(int(gravity))
+print()
+# int to str
+num_int = 10
+print('num_int', num_int)
+num_str = (str(num_int))
+print("num_str", num_str)
+print()
+# str to int or float
+num_str = '10.6'
+num_float = float(num_str)
+num_int = int(num_float)
+print('num_float', float(num_str))
+num_int = int(num_float)
+print('num_int', int(num_int))
+print()
+# str to list
+first_name = 'Byron'
+print(first_name)
+first_name_to_list = list(first_name)
+print(first_name_to_list)
---
+++ day_1/variables.py
@@ -0,0 +1,37 @@
+#Day 2 if python programming.
+first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on = "Byron", "Espinoza", "Byron Espinoza", "USA", "Houston", 37, 2026, True, True, False
+print(first_name)
+print(last_name)
+print(full_name)
+print(country)
+print(city)
+print(age)
+print(2026)
+print(is_married)
+print(is_light_on)
+print()
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_light_on))
+print()
+print(len(first_name))
+print(len(last_name))
+num_one, num_two = 5, 4
+print(num_one + num_two)
+tot_num = num_one + num_two
+print(tot_num)
+print(num_two - num_one)
+num_diff = num_two - num_one
+print(num_diff)
+num_prod = num_two * num_one
+print(num_prod)
+num_div = num_one / num_two
+print(num_div)
+num_rem = num_two % num_one
+print(num_rem)
